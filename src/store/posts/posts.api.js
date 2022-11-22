db41443f779082926c0c8de12a3c0e11ba125b16
Vladimir Vinogradov <v.v.vinogradov99@gmail.com>
2022-11-22 19:35:00 +0300

    Migrated the API

--- src/store/posts/posts.api.js
+++ src/store/posts/posts.api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const postsApi = createApi({
   reducerPath: 'api/latest',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'https://st-news-api-production.up.railway.app/',
+    baseUrl: 'https://api-production-1d72.up.railway.app/',
     mode: 'cors',
   }),
   endpoints: (build) => ({
